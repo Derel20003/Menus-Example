968cb3a7a8028515038ba177c3cf6bbe91ccd0a0
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2021-03-08 10:24:01 +0100

    Added all necessary objects, finished scene

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,36 +15,71 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage){
 
-        BorderPane root = new BorderPane();
+        VBox root = new VBox();
         root.setPrefSize(900, 500);
 
+        // Menu
         MenuBar mainMenu = new MenuBar();
-
         Menu datei = new Menu("Datei");
         Menu bearbeiten = new Menu("Bearbeiten");
-
         MenuItem schliessen = new MenuItem("Schließen");
+        schliessen.setOnAction(actionEvent -> Platform.exit());
         MenuItem ausschneiden = new MenuItem("Ausschneiden");
         MenuItem kopieren = new MenuItem("Kopieren");
         MenuItem einfuegen = new MenuItem("Einfügen");
-
         datei.getItems().add(schliessen);
         bearbeiten.getItems().addAll(ausschneiden, kopieren, einfuegen);
-
         mainMenu.getMenus().addAll(datei, bearbeiten);
 
+        // Toolbar
         ToolBar toolBar = new ToolBar();
         Button schliessenTool = new Button("Schließen");
+        schliessenTool.setOnMouseClicked(mouseEvent -> Platform.exit());
         Button ausschneidenTool = new Button("Ausschneiden");
         Button kopierenTool = new Button("Kopieren");
         Button einfuegenTool = new Button("Einfügen");
-        toolBar.getItems().addAll(schliessenTool, ausschneidenTool, kopierenTool, einfuegenTool);
-        toolBar.setOrientation(Orientation.VERTICAL);
-        VBox vBox = new VBox(toolBar);
+        toolBar.getItems().addAll(schliessenTool, new Separator(), ausschneidenTool, kopierenTool, einfuegenTool);
+        toolBar.setOrientation(Orientation.HORIZONTAL);
+        HBox toolBox = new HBox(toolBar);
+        toolBar.setPrefWidth(root.getPrefWidth());
+        toolBox.setLayoutY(30);
 
-        //root.setTop(mainMenu);
-        root.getChildren().add(vBox);
+        // TabPanes
+        TabPane tabPane = new TabPane();
+        Tab sourceTarget = new Tab("Source/Target");
+        Tab logging = new Tab("Logging");
+        sourceTarget.setClosable(false);
+        logging.setClosable(false);
 
+        TextArea source = new TextArea();
+        source.setPrefSize(300, 300);
+        source.setMaxSize(300, 300);
+        source.setLayoutX(100);
+        source.setLayoutY(50);
+
+        TextArea target = new TextArea();
+        target.setPrefSize(300, 300);
+        target.setMaxSize(300, 300);
+        target.setLayoutX(500);
+        target.setLayoutY(50);
+
+        TextArea log = new TextArea();
+        log.setPrefSize(600, 300);
+        log.setMaxSize(600, 300);
+        log.setLayoutX(150);
+        log.setLayoutY(50);
+
+        Pane paneSourceTarget = new Pane(source, target);
+        Pane paneLogging = new Pane(log);
+        sourceTarget.setContent(paneSourceTarget);
+        logging.setContent(paneLogging);
+
+        tabPane.getTabs().addAll(sourceTarget, logging);
+
+        // Add Menu, Toolbar to root
+        root.getChildren().addAll(mainMenu, toolBox, tabPane);
+
+        // Finish scene
         Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
